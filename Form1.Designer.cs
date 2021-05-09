2431981a1357019c256c08273189f2de2704e67e
Irineu Filho <54905683+filhoirineu@users.noreply.github.com>
2021-05-09 13:08:51 -0300

    Adicionando Projeto

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace gerenciados_atalhos
+{
+    partial class telaPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(telaPrincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cBoxAmbiente = new System.Windows.Forms.ComboBox();
+            this.cBoxConexao = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.cBoxPrograma = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cBoxTipoAmbiente = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-168, -81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ATALHOS TOTVS-PROTHEUS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(211, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "COMUNICAÇÃO NO CLIENTE:";
+            // 
+            // cBoxAmbiente
+            // 
+            this.cBoxAmbiente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxAmbiente.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cBoxAmbiente.Location = new System.Drawing.Point(12, 329);
+            this.cBoxAmbiente.Name = "cBoxAmbiente";
+            this.cBoxAmbiente.Size = new System.Drawing.Size(360, 26);
+            this.cBoxAmbiente.TabIndex = 9;
+            // 
+            // cBoxConexao
+            // 
+            this.cBoxConexao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxConexao.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cBoxConexao.Location = new System.Drawing.Point(12, 256);
+            this.cBoxConexao.Name = "cBoxConexao";
+            this.cBoxConexao.Size = new System.Drawing.Size(360, 26);
+            this.cBoxConexao.TabIndex = 7;
+            this.cBoxConexao.SelectedIndexChanged += new System.EventHandler(this.cBoxConexao_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 310);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(188, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "AMBIENTE NO SERVIDOR:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PROGRAMA INICIAL:";
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.btnExecutar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnExecutar.ForeColor = System.Drawing.Color.Black;
+            this.btnExecutar.Location = new System.Drawing.Point(12, 384);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(360, 35);
+            this.btnExecutar.TabIndex = 11;
+            this.btnExecutar.Text = "EXECUTAR";
+            this.btnExecutar.UseVisualStyleBackColor = false;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // cBoxPrograma
+            // 
+            this.cBoxPrograma.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxPrograma.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cBoxPrograma.Location = new System.Drawing.Point(12, 183);
+            this.cBoxPrograma.Name = "cBoxPrograma";
+            this.cBoxPrograma.Size = new System.Drawing.Size(360, 26);
+            this.cBoxPrograma.TabIndex = 5;
+            this.cBoxPrograma.SelectedIndexChanged += new System.EventHandler(this.cBoxPrograma_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "AMBIENTE DE EXECUÇÃO:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(180, 422);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(192, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "dúvidas? ti@paranoa.com.br";
+            // 
+            // cBoxTipoAmbiente
+            // 
+            this.cBoxTipoAmbiente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxTipoAmbiente.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cBoxTipoAmbiente.Items.AddRange(new object[] {
+            "PRODUCAO",
+            "DESENVOLVIMENTO",
+            "HOMOLOGACAO",
+            "VENDAS",
+            "TKM-HISTORICO"});
+            this.cBoxTipoAmbiente.Location = new System.Drawing.Point(12, 110);
+            this.cBoxTipoAmbiente.Name = "cBoxTipoAmbiente";
+            this.cBoxTipoAmbiente.Size = new System.Drawing.Size(360, 26);
+            this.cBoxTipoAmbiente.TabIndex = 0;
+            this.cBoxTipoAmbiente.SelectedIndexChanged += new System.EventHandler(this.cBoxTipoAmbiente_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(197, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(175, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // telaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(384, 444);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cBoxAmbiente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cBoxConexao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cBoxPrograma);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cBoxTipoAmbiente);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(32)))), ((int)(((byte)(96)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "telaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PARANOÁ | TOTVS SmartClient";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cBoxAmbiente;
+        private System.Windows.Forms.ComboBox cBoxConexao;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ComboBox cBoxPrograma;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cBoxTipoAmbiente;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace gerenciados_atalhos
+{
+    public partial class telaPrincipal : Form
+    {
+        public telaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            String cComandos = "-m -P=%PROGRAMA% -C=%CONEXAO% -E=%AMBIENTE%";
+            String cProgExec = "";
+            String cTipoAmbiente = "";
+            String cPrograma = "";
+            String cConexao = "";
+            String cAmbiente = "";
+            int nIndexTipoAmbiente = cBoxTipoAmbiente.SelectedIndex;
+            int nIndexAmbiente = cBoxAmbiente.SelectedIndex;
+            int nIndexConexao = cBoxConexao.SelectedIndex;
+            int nIndexPrograma = cBoxPrograma.SelectedIndex;
+
+            if (nIndexTipoAmbiente >= 0 && nIndexAmbiente >= 0 && nIndexConexao >= 0 && nIndexPrograma >= 0)
+            {
+                cTipoAmbiente = cBoxTipoAmbiente.SelectedItem.ToString();
+                cPrograma = cBoxPrograma.SelectedItem.ToString();
+                cAmbiente = cBoxAmbiente.SelectedItem.ToString();
+                cConexao = cBoxConexao.SelectedItem.ToString();
+
+                if (cTipoAmbiente == "PRODUCAO")
+                {
+                    cProgExec = "C:\\Totvs\\Protheus12_prd\\smartclient.exe";
+                }
+                else if (cTipoAmbiente == "DESENVOLVIMENTO")
+                {
+                    cProgExec = "C:\\Totvs\\Protheus12_DEV\\smartclient.exe";
+                }
+                else if (cTipoAmbiente == "HOMOLOGACAO")
+                {
+                    cProgExec = "C:\\Totvs\\Protheus12_HOM\\smartclient.exe";
+                }
+                else if (cTipoAmbiente == "VENDAS")
+                {
+                    cProgExec = "C:\\Totvs\\Protheus12_VEN\\smartclient.exe";
+                }
+                else
+                {
+                    cProgExec = "C:\\Totvs\\Protheus11_tkm\\SmartClient.exe";
+                }
+
+                if (cPrograma == "MATRIZ DE VERSATILIDADE")
+                {
+                    cPrograma = "u_PZY3VIEW";
+                }
+                else if (cPrograma == "CHAMADOS DE TI")
+                {
+                    cPrograma = "u_PCHMDSTI";
+                }
+                else if (cPrograma == "TELNET")
+                {
+                    cProgExec = "C:\\Windows\\System32\\telnet.exe";
+                }
+
+                if (cAmbiente == "SPLASH")
+                {
+                    cComandos = "-m";
+                }
+                else if (cAmbiente == "TELNET")
+                {
+                    cComandos = "192.168.0.31 24";
+                }
+                else
+                {
+                    cComandos = cComandos.Replace("%PROGRAMA%", cPrograma);
+                    cComandos = cComandos.Replace("%CONEXAO%", cConexao);
+                    cComandos = cComandos.Replace("%AMBIENTE%", cAmbiente);
+                }
+
+                if (File.Exists(cProgExec))
+                {
+
+                    System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+                    pProcess.StartInfo.FileName = cProgExec;
+                    pProcess.StartInfo.Arguments = cComandos;
+                    pProcess.Start();
+                    telaPrincipal.ActiveForm.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Programa não encontrado! \r\n" + cProgExec, "ATALHOS TOTVS-PROTHEUS");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Selecionar todos os parametros para execução", "ATALHOS TOTVS-PROTHEUS");
+            }
+
+        }
+
+        private void cBoxTipoAmbiente_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+
+            cBoxPrograma.Items.Clear();
+            cBoxConexao.Items.Clear();
+            cBoxAmbiente.Items.Clear();
+
+            cBoxPrograma.Items.Add("SIGAMDI");
+            cBoxPrograma.Items.Add("SIGAADV");
+
+            if (cBoxTipoAmbiente.SelectedItem.ToString() == "PRODUCAO")
+            {
+                cBoxPrograma.Items.Add("CHAMADOS DE TI");
+                cBoxPrograma.Items.Add("MATRIZ DE VERSATILIDADE");
+                cBoxPrograma.Items.Add("TELNET");
+            }
+
+        }
+
+        private void cBoxPrograma_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+
+            cBoxConexao.Items.Clear();
+            cBoxConexao.Enabled = true;
+            cBoxAmbiente.Items.Clear();
+            cBoxAmbiente.Enabled = true;
+
+            if (cBoxPrograma.SelectedItem.ToString() == "CHAMADOS DE TI" || cBoxPrograma.SelectedItem.ToString() == "MATRIZ DE VERSATILIDADE")
+            {
+                cBoxConexao.Items.Add("TCP");
+                cBoxConexao.SelectedIndex = 0;
+                cBoxConexao.Enabled = false;
+
+                cBoxAmbiente.Items.Add("PARANOA");
+                cBoxAmbiente.SelectedIndex = 0;
+                cBoxAmbiente.Enabled = false;
+            }
+
+            else if (cBoxPrograma.SelectedItem.ToString() == "TELNET")
+            {
+                cBoxConexao.Items.Add("TELNET");
+                cBoxConexao.SelectedIndex = 0;
+                cBoxConexao.Enabled = false;
+
+                cBoxAmbiente.Items.Add("TELNET");
+                cBoxAmbiente.SelectedIndex = 0;
+                cBoxAmbiente.Enabled = false;
+            }
+            else if (cBoxPrograma.SelectedItem.ToString() == "SIGAADV" || cBoxPrograma.SelectedItem.ToString() == "SIGAMDI")
+            {
+
+                cBoxConexao.Items.Add("TCP");
+                cBoxConexao.Items.Add("EXTERNO");
+                cBoxConexao.SelectedIndex = 0;
+
+                if (cBoxTipoAmbiente.SelectedItem.ToString() == "PRODUCAO")
+                {
+                    cBoxConexao.Items.Add("SERVICE10");
+                    cBoxConexao.Items.Add("SERVICE15");
+                    cBoxConexao.Items.Add("SERVICE16");
+                    cBoxConexao.Items.Add("SERVICE17");
+                    cBoxConexao.Items.Add("SERVICE18");
+                    cBoxConexao.Items.Add("SERVICE19");
+                    cBoxConexao.Items.Add("SERVICE20");
+                    cBoxConexao.Items.Add("SERVICE26");
+                    cBoxConexao.Items.Add("SERVICE27");
+                    cBoxConexao.Items.Add("SERVICE28");
+                }
+            }
+
+        }
+
+        private void cBoxConexao_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+
+            cBoxAmbiente.Items.Clear();
+
+            if (cBoxTipoAmbiente.SelectedItem.ToString() == "PRODUCAO")
+            {
+                if (cBoxConexao.SelectedItem.ToString() == "TELNET")
+                {
+                    cBoxAmbiente.Items.Add("TELNET");
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "TCP" || cBoxConexao.SelectedItem.ToString() == "EXTERNO")
+                {
+                    cBoxAmbiente.Items.Add("PARANOA");
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE10")
+                {
+                    cBoxAmbiente.Items.Add("PARANOA");
+                    cBoxAmbiente.Items.Add("PARANOA_CST");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE15")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_E");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE16")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_I");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE17")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_J");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE18")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_R");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE19")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_S");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE20")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_M");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE26")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_RK");
+
+                }
+                else if (cBoxConexao.SelectedItem.ToString() == "SERVICE27")
+                {
+                    cBoxAmbiente.Items.Add("ANALISTA_A");
+
+                }
+
+            }
+            else if (cBoxTipoAmbiente.SelectedItem.ToString() == "DESENVOLVIMENTO")
+            {
+                cBoxAmbiente.Items.Add("DESENV");
+            }
+            else if (cBoxTipoAmbiente.SelectedItem.ToString() == "HOMOLOGACAO")
+            {
+                cBoxAmbiente.Items.Add("HOMOLOG");
+            }
+            else if (cBoxTipoAmbiente.SelectedItem.ToString() == "VENDAS")
+            {
+                cBoxAmbiente.Items.Add("VENDAS");
+            }
+            else if (cBoxTipoAmbiente.SelectedItem.ToString() == "TKM-HISTORICO")
+            {
+                cBoxAmbiente.Items.Add("P11");
+            }
+
+            cBoxAmbiente.Items.Add("SPLASH");
+            cBoxAmbiente.SelectedIndex = 0;
+        }
+
+    }
+}
